d11f3e2c175acc23b032c67c91e201be508d98f2
shoaib khan <82094195+shoaibkhan188626@users.noreply.github.com>
2025-03-01 23:00:29 +0530

    some logical changes to the voter controller

--- controller/voterController.js
+++ controller/voterController.js
@@ -2,81 +2,89 @@ import voterSchema from "../schema/voterSchema.js";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 
+//for getting all the voters in the database
 export const getAll = async (req, res) => {
   try {
     const voters = await voterSchema.find();
-    res.json(voters);
+    res.status(200).json({ success: true, count: voters.length, voters });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
 };
 
+//for getting a particular voter by it's voter ID.
 export const getVoterById = async (req, res) => {
   try {
     const voter = await voterSchema.findOne({ voterId: req.params.voterId });
-    if (!voter) return res.status(404).json({ message: "Voter not Found" });
-    res.json(voter);
+    if (!voter)
+      return res
+        .status(404)
+        .json({ success: false, message: "Voter not Found" });
+    res.status(200).json({ success: true, voter });
   } catch (error) {
-    res.status(500).json({ error: error.message });
+    res.status(500).json({ success: false, error: error.message });
   }
 };
 
 export const getByHouseNo = async (req, res) => {
   try {
     let { gharNo } = req.body;
+
     if (!gharNo || typeof gharNo !== "string") {
-      return res.status(400).json({ message: "gharNo must be a valid string" });
+      return res
+        .status(400)
+        .json({ success: false, message: "gharNo must be a valid string" });
     }
+
     const voters = await voterSchema.find({ gharNo });
+
     if (voters.length === 0) {
       return res
         .status(404)
         .json({ message: "No voters are assocaited with this House Number" });
     }
-    res.json(voters);
+
+    res.status(200).json({ success: true, count: voters.length, voters });
   } catch (error) {
-    res.status(500).json({ error: error.message });
+    res.status(500).json({ success: false, error: error.message });
   }
 };
 
 export const create = async (req, res) => {
   try {
-    const {
-      gharNo,
-      voterId,
-      name,
-      fatherName,
-      age,
-      address,
-      gender,
-      serialNumber,
-      yadiSerial,
-      amount,
-      comment,
-      updatedBy,
-      photo,
-    } = req.body;
-    const newVoter = new voterSchema({
-      name,
-      fatherName,
-      age,
-      gender,
-      gharNo,
-      address,
-      photo,
-      voterId,
-      yadiSerial,
-      serialNumber,
-      amount,
-      updatedBy,
-      comment,
-    });
-    await newVoter.save();
-    res
-      .status(200)
-      .json({ message: "Voter Created Successfully", voter: newVoter });
+    const requiredFields = [
+      "gharNo",
+      "voterId",
+      "name",
+      "fatherName",
+      "age",
+      "address",
+      "gender",
+      "serialNumber",
+      "yadiSerial",
+      "amount",
+      "comment",
+      "updatedBy",
+      "photo",
+    ];
+
+    for (const field of requiredFields) {
+      if (!req.body[field])
+        return res
+          .status(400)
+          .json({ success: false, message: `${field} is required` });
+
+      const newVoter = new voterSchema(req.body);
+      await newVoter.save();
+
+      res.status(201).json({
+        success: true,
+        message: "voter created successfully",
+        voter: newVoter,
+      });
+    }
   } catch (error) {
-    res.status(500).json({ error: error.message });
+    res.status(500).json({ success: false, error: error.message });
   }
 };
 
@@ -87,23 +95,37 @@ export const update = async (req, res) => {
       req.body,
       { new: true }
     );
+
     if (!updateVoter)
-      return res.status(404).json({ message: "Voter not Found" });
-    res.json(updateVoter);
+      return res
+        .status(404)
+        .json({ success: false, message: "Voter not Found" });
+
+    res.status(200).json({
+      success: true,
+      message: "voter Updated successfully",
+      voter: updateVoter,
+    });
   } catch (error) {
-    res.status(500).json({ error: error.message });
+    res.status(500).json({ success: false, error: error.message });
   }
 };
 
 export const remove = async (req, res) => {
   try {
     const { voterId } = req.params;
+
     const deletedVoter = await voterSchema.findOneAndDelete({ voterId });
     if (!deletedVoter)
-      return res.status(404).json({ message: "Voter Not Found" });
-    res.json({ message: "Voter Deleted Successfully" });
+      return res
+        .status(404)
+        .json({ success: false, message: "Voter Not Found" });
+
+    res
+      .status(200)
+      .json({ success: true, message: "Voter Deleted Successfully" });
   } catch (error) {
-    res.status(500).json({ error: error.message });
+    res.status(500).json({ success: false, error: error.message });
   }
 };
 
@@ -111,13 +133,20 @@ export const search = async (req, res) => {
   try {
     const { phoneNumber, name, voterId, gharNo } = req.body;
     const query = {};
+
     if (phoneNumber) query.phoneNumber = phoneNumber;
     if (name) query.name = new RegExp(name, "i");
     if (voterId) query.voterId = voterId;
     if (gharNo) query.gharNo = gharNo;
+
     const voter = await voterSchema.find(query);
-    res.json(voter);
+
+    if (!voter.length)
+      return res
+        .status(404)
+        .json({ success: false, message: "no voters found" });
+    res.status(200).json({ success: true, count: voter.length, voter });
   } catch (error) {
-    res.status(500).json({ error: error.message });
+    res.status(500).json({ success: false, error: error.message });
   }
 };
